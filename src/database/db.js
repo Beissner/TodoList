98b7cd28cb89123a48422e18b3a81cece6b69b11
Emily Beissner <kebeissner@gmail.com>
2020-09-10 21:56:28 +0100

    Add env variable for production

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-const db = config.get('mongoURI');
+const db = process.env.MONGODB_URI ||config.get('mongoURI');
 
 const connectDB = async () => {
     try {
@@ -11,6 +11,6 @@ const connectDB = async () => {
         console.error(err.message);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
