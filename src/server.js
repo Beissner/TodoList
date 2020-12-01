263fb1deca7057421474db802e0553bc54a37c53
Emily Beissner <kebeissner@gmail.com>
2020-12-01 13:32:56 +0000

    adjust port for production in server file

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const connectDB = require('./database/db');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 5000;
 
 // connect database
 connectDB();
